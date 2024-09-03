044850cd54789365b93ab47954734ddf5c333655
Jared <jaredgomez0812@gmail.com>
2024-09-03 15:29:55 -0400

    s

--- config.ts
+++ config.ts
@@ -1,5 +1,13 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-export { API_BASE_URL };
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+// export { API_BASE_URL };
 
-// const API_BASE_URL =
-//   process.env.REACT_APP_API_BASE_URL
+// // const API_BASE_URL =
+// //   process.env.REACT_APP_API_BASE_URL
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+if (!API_BASE_URL) {
+  throw new Error(
+    "API_BASE_URL is not defined. Check your environment variables."
+  );
+}
+export { API_BASE_URL };
